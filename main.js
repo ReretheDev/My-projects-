efa8e0d97079e3e746b83560fb65d71aff42fa82
ReretheDev <141488449+ReretheDev@users.noreply.github.com>
2023-08-06 01:49:05 +0100

    Add files via upload
    Palindrome Checker

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-let kgRef = document.getElementById("kg");
-let ozRef = document.getElementById("oz");
-let lbRef = document.getElementById("lb");
-let convertFromKg = () => {
-  let kg = kgRef.value;
-  lbRef.value = (kg * 2.205).toFixed(2);
-  ozRef.value = (kg * 35.274).toFixed(2);
-};
-let convertFromLb = () => {
-  let lb = lbRef.value;
-  kgRef.value = (lb / 2.205).toFixed(2);
-  ozRef.value = (lb * 16).toFixed(2);
-};
-let convertFromOz = () => {
-  let oz = ozRef.value;
-  kgRef.value = (oz / 35.274).toFixed(2);
-  lbRef.value = (oz / 16).toFixed(2);
-};
-
-kgRef.addEventListener("input", convertFromKg);
-lbRef.addEventListener("input", convertFromLb);
-ozRef.addEventListener("input", convertFromOz);
-
-
+let btn = document.querySelector("button");
 let loader = document.querySelector("#loader");
-window.addEventListener("load", function() {
-  convertFromKg();
-  setTimeout(() => {
-    loader.style.display = "none"
-  },4000)
+
+let inputText = document.querySelector("#input-text");
+let result = document.querySelector("#result");
+btn.addEventListener("click", () => {
+  const text = inputText.value.trim();
+  if(text.length == 0) {
+    alert("Input field can not be empty");
+    return;
+  }
+  const cleanText = text.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  
+  const isPalindrome = cleanText === cleanText.split("").reverse("").join("");
+const message = isPalindrome ? `<span>Yes.</span> It is a palindrome!` :
+`<span>No.</span> It is not a palindrome!` ;
+result.innerHTML = message;
+result.classList.remove("error", "success");
+setTimeout( () =>{
+  result.classList.add(isPalindrome ? "success" : "error" )
+},10)
 });
+window.addEventListener("load", function(){
+ setTimeout(()=>{
+   loader.style.display ="none"
+ },4000)
+})
